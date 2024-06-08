1b0ecd617d23607a883980144b4002b30499fb7e
divyanshgoyal777 <tonisha24680@gmail.com>
2024-06-08 14:17:25 +0530

    update

--- Spotify_Clone/js file/script.js
+++ Spotify_Clone/js file/script.js
@@ -28,8 +28,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`https://github.com/divyanshgoyal777/Spotify/tree/main/Spotify_Clone/${folder}/`, {mode: 'no-cors'});
-    console.log(a)
+    let a = await fetch(`/Spotify_Clone/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -77,8 +76,7 @@ const playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-    let a = await fetch(`https://github.com/divyanshgoyal777/Spotify/tree/main/Spotify_Clone/song/`, {mode: 'no-cors'});
-    console.log(a)
+    let a = await fetch(`/Spotify_Clone/song/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -90,8 +88,7 @@ async function displayAlbums() {
 
         if (e.href.includes("/song")) {
             let folder = e.href.split("/").slice(-2)[0];
-            let a = await fetch(`https://github.com/divyanshgoyal777/Spotify/tree/main/Spotify_Clone/song/${folder}/info.json`, {mode: 'no-cors'});
-            console.log(a)
+            let a = await fetch(`/Spotify_Clone/song/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
                                                                     <div class="play">
